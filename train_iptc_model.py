bc718896db5f003f42349ec019952475a7a59fde
Your Name <ec.cappon@gmail.com>
2024-02-19 21:56:11 +0100

    slice

--- train_iptc_model.py
+++ train_iptc_model.py
@@ -13,7 +13,7 @@ from transformers import (
 # ------------------------------------
 ## load data
 
-df = pd.read_csv("iptc_train_data_example.csv")[:40]
+df = pd.read_csv("iptc_train_data_example.csv")[:20]
 
 # ------------------------------------
 ## prep data
